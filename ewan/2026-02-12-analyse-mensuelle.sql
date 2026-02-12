25382c76ace57f24a94a79093c1ec9b7b26ddfd2
Antonio <antoniorandria3342@gmail.com>
2026-02-12 11:54:30 +0300

    tsy mety mihitsy le somme fa aleo andeha amty

---
+++ ewan/2026-02-12-analyse-mensuelle.sql
@@ -0,0 +1,42 @@
+-- Modification de la vue VENTE_DETAILS_CPL_2_VISEE pour ajouter la colonne moisAnnee
+-- Date: 2026-02-12
+-- Auteur: Modification pour analyse mensuelle
+
+CREATE OR REPLACE VIEW VENTE_DETAILS_CPL_2_VISEE AS
+SELECT vd.ID,
+       vd.IDVENTE,
+       v.DESIGNATION          AS IDVENTELIB,
+       vd.IDPRODUIT ,
+       i.LIBELLE                 AS IDPRODUITLIB,
+       vd.IDORIGINE,
+       vd.QTE,
+       nvl(vd.PU, 0)          AS PU,
+       CAST(nvl((vd.PU * vd.QTE)-nvl(vd.REMISE,0), 0) +nvl(((vd.PU * vd.QTE)-nvl(vd.REMISE,0)) * (vd.TVA/100), 0) AS number(30,2)) AS puTotal,
+       CAST(nvl(vd.PUACHAT * vd.QTE, 0) AS number(30,2)) AS puTotalAchat,
+       CAST(nvl(vd.PU * vd.QTE, 0) - nvl(vd.PUACHAT * vd.QTE, 0) AS number(30,2)) AS puRevient,
+       c.ID  AS IDCATEGORIE,
+       c.VAL AS IDCATEGORIELIB,
+       v.DATY AS daty,
+       m.ID AS IDMAGASIN,
+       m.VAL AS IDMAGASINLIB,
+       p1.ID AS IDPOINT,
+       p1.VAL AS IDPOINTLIB,
+       vd.IDDEVISE,
+       vd.IDDEVISE AS IDDEVISELIB,
+       cast(nvl((CAST(nvl(vd.PU * vd.QTE*(1-nvl(vd.REMISE,0)/100), 0) +nvl(vd.PU * vd.QTE*(1-nvl(vd.REMISE,0)/100) *(vd.TVA/100), 0) AS number(30,2))-(vd.QTE*vd.PUREVIENT)),0) as number(20,2)) as margeBrute,
+       v.IDRESERVATION,
+       s.ID AS IDSUPPORT,
+       s.VAL AS IDSUPPORTLIB,
+       TO_CHAR(v.DATY, 'MM/YY') AS moisAnnee,
+       TO_CHAR(v.DATY, 'YYYY') AS annee,
+       TO_NUMBER(TO_CHAR(v.DATY, 'YYYYMM')) AS moisAnneeTri
+FROM VENTE_DETAILS vd
+         LEFT JOIN VENTE v ON v.ID = vd.IDVENTE
+         LEFT JOIN MAGASIN m ON m.ID = v.IDMAGASIN
+         LEFT JOIN AS_INGREDIENTS i ON i.ID = vd.IDPRODUIT
+         LEFT JOIN POINT p1 ON p1.ID = m.IDPOINT
+         LEFT JOIN CATEGORIEINGREDIENT c  ON i.CATEGORIEINGREDIENT  = c.ID
+         LEFT JOIN SUPPORT s ON s.ID = i.IDSUPPORT
+WHERE v.ETAT >= 11;
+
+COMMIT;
